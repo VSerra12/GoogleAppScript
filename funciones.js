863d90a28d08c4cb6e713e8e803e4e531eaabfd9
VSerra12 <vicky.serra.24@gmail.com>
2024-10-28 20:07:54 -0300

    cargar contactos desde google sheets

--- funciones.js
+++ funciones.js
@@ -5,3 +5,10 @@ function doGet(){
 function obtenerDatosHtml(nombre){
     return HtmlService.createHtmlOutputFromFile(nombre).getContent()
 }
+
+function obtenerContactos(){
+    let hoja = SpreadsheetApp.openById('1lqVAS07nibQuq2sMvhbm5XHw8K1oYroL6zXw1GUw5nE').getActiveSheet()
+    let datos = hoja.getDataRange().getValues()
+
+    return datos
+}
